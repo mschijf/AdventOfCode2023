0a2a1d151b4105290dbba0b453b303837e8dca3e
Martin Schijf <81872457+mschijf@users.noreply.github.com>
2023-12-02 07:04:09 +0100

    Day 2 - refactor

--- src/main/kotlin/adventofcode2023/Day02.kt
+++ src/main/kotlin/adventofcode2023/Day02.kt
@@ -2,13 +2,10 @@ package adventofcode2023
 
 import tool.mylambdas.substringBetween
 import kotlin.math.max
-import kotlin.math.min
 
 fun main() {
     Day02(test=false).showResult()
 }
-
-//49 is wrong
 
 class Day02(test: Boolean) : PuzzleSolverAbstract(test) {
 
@@ -16,36 +13,38 @@ class Day02(test: Boolean) : PuzzleSolverAbstract(test) {
 
     //12 red cubes, 13 green cubes, and 14 blue cubes
     override fun resultPartOne(): Any {
+        val possibilityCheck = mapOf("red" to 12, "green" to 13, "blue" to 14)
         return gameList
-            .filter { it.isPossibleFor(mapOf("red" to 12, "green" to 13, "blue" to 14)) }
+            .filter { game -> game.isPossibleFor(possibilityCheck) }
             .sumOf { it.id }
     }
 
     override fun resultPartTwo(): Any {
-        return gameList.sumOf { it.power() }
+        return gameList
+            .sumOf { it.power() }
     }
 }
 
 
-data class Game(val id: Int, val revealedList: List<Reveal>) {
+data class Game(val id: Int, val grabList: List<Grab>) {
     companion object {
         //Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-        fun of(raw: String): Game {
-            val id = raw.substringBetween("Game", ":").trim().toInt()
-            val ll = raw.substringAfter(": ").split("; ").map{Reveal.of(it)}
-            return Game(id, ll)
-        }
+        fun of(raw: String) =
+            Game (
+                id = raw.substringBetween("Game", ":").trim().toInt(),
+                grabList = raw.substringAfter(": ").split("; ").map{Grab.of(it)}
+            )
     }
 
     fun isPossibleFor(configuration: Map<String, Int>)=
-        revealedList.all {it.isPossibleFor(configuration)}
+        grabList.all {it.isPossibleFor(configuration)}
 
     private fun minimalSet(): Map<String, Long> {
         val result = mutableMapOf<String, Long>()
-        revealedList.forEach { revealed ->
-            revealed.revealed.forEach {(color, colorCount) ->
-                val currentMinimum = result.getOrDefault(color, 0)
-                result.put(color, max(currentMinimum.toLong(), colorCount.toLong()))
+        grabList.forEach { aGrab ->
+            aGrab.cubeCountPerColor.forEach { (color, colorCount) ->
+                val currentMinimum = result.getOrDefault(color, 0L)
+                result[color] = max(currentMinimum, colorCount.toLong())
             }
         }
         return result
@@ -54,22 +53,21 @@ data class Game(val id: Int, val revealedList: List<Reveal>) {
     fun power(): Long {
         return minimalSet().values.reduce { acc, l -> acc*l }
     }
+
+
 }
 
-data class Reveal(val revealed: List<Pair<String, Int>>) {
+data class Grab(val cubeCountPerColor: Map<String, Int>) {
     companion object {
         //3 blue, 4 red
-        fun of(raw: String): Reveal {
-            val ll = raw.split(", ").map{Pair(it.substringAfter(" "), it.substringBefore(" ").trim().toInt())}
-            return Reveal(ll)
-        }
+        fun of(raw: String) =
+            Grab (
+                cubeCountPerColor = raw
+                    .split(", ")
+                    .associate{it.substringAfter(" ") to it.substringBefore(" ").trim().toInt()}
+            )
     }
 
-    fun isPossibleFor(configuration: Map<String, Int>): Boolean {
-        revealed.forEach { (color, colorCount) ->
-            if (configuration.getOrDefault(color, -1) < colorCount)
-                return false
-        }
-        return true
-    }
+    fun isPossibleFor(configuration: Map<String, Int>) =
+        cubeCountPerColor.none { (color, colorCount) -> configuration.getOrDefault(color, -1) < colorCount }
 }
