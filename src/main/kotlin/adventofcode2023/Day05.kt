a5605886ce9fd943bb9734e2c2cee5545105270e
Martin Schijf <81872457+mschijf@users.noreply.github.com>
2023-12-05 19:40:12 +0100

    Day 5: refactoring. Can be (much) better, but out of inspiration

--- src/main/kotlin/adventofcode2023/Day05.kt
+++ src/main/kotlin/adventofcode2023/Day05.kt
@@ -8,58 +8,156 @@ fun main() {
     Day05(test=false).showResult()
 }
 
-// Bruteforce does work...
+// Bruteforce does work (see partTwoBruteForce)...
 //   Result part 1: 388071289 (after 0.001 sec)
 //   Result part 2: 84206669 (after 525.693 sec)
-//                 218728775
+//
 
-// 0..105449248
-//    218728775
+/*
+Deel twee is niet de mooiste oplossing, maar wel een manier om het efficient te doen:
+- bepaal van de laatste transformer ('humidity-to-location') wat de laagste destinationRange is. In principe zit daar
+  de laagste location in.
+- neem van die destinationRange, de bijbehorende sourceRange
+- ga op basis van die sourceRange kijken welke destinationRanges er in 'temperature-to-humidity' zijn te vinden
+  (dit wordt een lijst van ranges)
+- neem daar weer de bijbehorende lijst van sourceRanges van
+- gebruik die sourceRanges lijst weer als destination in 'light-to-temperature'
+- etc. tot je aan het begin bent en een lijst van seed-ranges hebt.
+
+-    met behulp daarvan gaan we voor ieder element van die ranges die ook voorkomt in de start-seed ranges het
+     proces van deel 1 doen en bepalen zo de kleinste
+ */
+
+// (de laagste destination kan ook een 0..x range zijn, met x de laagste startpunt van alle destination ranges
 
 class Day05(test: Boolean) : PuzzleSolverAbstract(test) {
 
-    val seedList = inputLines().first().substringAfter("seeds: ").split("\\s+".toRegex()).map{it.toLong()}
-    val seedRanges = seedList.chunked(2).map{it[0]..it[0]+it[1]-1}
-    val transformerList = inputLines().drop(2).splitByCondition { it.isEmpty() }.map{Transformer.of(it)}
-    val transformerMap = transformerList.associateBy { it.source }
+    val seedList = inputLines()
+        .first()
+        .substringAfter("seeds: ")
+        .split("\\s+".toRegex()).map{it.toLong()}
 
-//    override fun resultPartOne(): Any {
-//        return seedList.map{seedToLocation(it)}.min()
-//    }
+    val seedRanges = seedList
+        .chunked(2)
+        .map{it[0]..it[0]+it[1]-1}
 
-    override fun resultPartTwo(): Any {
-        var nextRanges = if (test)
-            listOf((0L..55))
-        else
-            listOf(transformerMap["humidity"]!!.transformNumberList.first { 0 in it.destinationRange }!!.sourceRange)
+    val transformerMap = inputLines()
+        .drop(2)
+        .splitByCondition { it.isEmpty() }
+        .map{ Transformer.of(it) }
+        .associateBy { it.source }
 
-        println(nextRanges.sortedBy { it.first })
-        nextRanges = nextRanges.flatMap{ aRange -> destinationRangesToSource(aRange, transformerMap["temperature"]!!.transformNumberList )}
-        println(nextRanges.sortedBy { it.first })
-        nextRanges = nextRanges.flatMap{ aRange -> destinationRangesToSource(aRange, transformerMap["light"]!!.transformNumberList )}
-        println(nextRanges.sortedBy { it.first })
-        nextRanges = nextRanges.flatMap{ aRange -> destinationRangesToSource(aRange, transformerMap["water"]!!.transformNumberList )}
-        println(nextRanges.sortedBy { it.first })
-        nextRanges = nextRanges.flatMap{ aRange -> destinationRangesToSource(aRange, transformerMap["fertilizer"]!!.transformNumberList )}
-        println(nextRanges.sortedBy { it.first })
-        nextRanges = nextRanges.flatMap{ aRange -> destinationRangesToSource(aRange, transformerMap["soil"]!!.transformNumberList )}
-        println(nextRanges.sortedBy { it.first })
-        nextRanges = nextRanges.flatMap{ aRange -> destinationRangesToSource(aRange, transformerMap["seed"]!!.transformNumberList )}
-        println(nextRanges.sortedBy { it.first })
-
-        var smallest = Long.MAX_VALUE
-        nextRanges.forEach { seedRange ->
-            var i = seedRange.first
-            while (i <= seedRange.last) {
-                if (i.inRanges(seedRanges))
-                    smallest = min(smallest, seedToLocation(i))
-                i++
-            }
-        }
-        return smallest
-
+    override fun resultPartOne(): Any {
+        return seedList
+            .map{ seedToLocation(it) }
+            .min()
     }
 
+
+    override fun resultPartTwo(): Any {
+        val finalTransformer = transformerMap["humidity"]!!.fillEmptyRangesInDestination() //to add 0..x ranges in beginning
+
+        finalTransformer.transformNumberList.sortedBy { it.destinationRange.first }.forEach {
+            var sourceRangeList = listOf(it.sourceRange)
+            sourceRangeList = sourceRangeList.flatMap{ aRange -> destinationRangesToSource(aRange, transformerMap["temperature"]!!.transformNumberList )}
+            sourceRangeList = sourceRangeList.flatMap{ aRange -> destinationRangesToSource(aRange, transformerMap["light"]!!.transformNumberList )}
+            sourceRangeList = sourceRangeList.flatMap{ aRange -> destinationRangesToSource(aRange, transformerMap["water"]!!.transformNumberList )}
+            sourceRangeList = sourceRangeList.flatMap{ aRange -> destinationRangesToSource(aRange, transformerMap["fertilizer"]!!.transformNumberList )}
+            sourceRangeList = sourceRangeList.flatMap{ aRange -> destinationRangesToSource(aRange, transformerMap["soil"]!!.transformNumberList )}
+            sourceRangeList = sourceRangeList.flatMap{ aRange -> destinationRangesToSource(aRange, transformerMap["seed"]!!.transformNumberList )}
+
+            var smallest = Long.MAX_VALUE
+            sourceRangeList.forEach { seedRange ->
+                var i = seedRange.first
+                while (i <= seedRange.last) {
+                    if (i.inRanges(seedRanges))
+                        smallest = min(smallest, seedToLocation(i))
+                    i++
+                }
+            }
+            if (smallest < Long.MAX_VALUE)
+                return smallest
+        }
+        return -1L
+    }
+
+
+    private fun seedToLocation(seedNumber: Long): Long {
+        var current = "seed"
+        var currentNumber = seedNumber
+
+        while (transformerMap.contains(current)) {
+            currentNumber = transformerMap[current]!!.transform(currentNumber)
+            current = transformerMap[current]!!.destination
+        }
+        return currentNumber
+    }
+
+    private fun Long.inRanges(checkRanges: List<LongRange>) : Boolean {
+        return checkRanges.any{range -> this in range}
+    }
+
+    private fun destinationRangesToSource(inRange: LongRange, compareRanges: List<TransformNumber>): List<LongRange> {
+        val result = mutableListOf<LongRange>()
+        var start = inRange.first
+        var end = inRange.last
+        compareRanges.forEach {compareRange ->
+            if (start <= end && start in compareRange.destinationRange && end in compareRange.destinationRange) {
+                result.add(compareRange.sourceRange.first + (start-compareRange.destinationRange.first)..compareRange.sourceRange.first + (end-compareRange.destinationRange.first))
+                return result
+            }
+            if (start <= end && start in compareRange.destinationRange) {
+                result.add(compareRange.sourceRange.first + (start-compareRange.destinationRange.first)..compareRange.sourceRange.last)
+                start = compareRange.destinationRange.last+1
+            }
+            if (start <= end && end in compareRange.destinationRange) {
+                result.add(compareRange.sourceRange.first ..compareRange.sourceRange.first + (end-compareRange.destinationRange.first))
+                end = compareRange.destinationRange.first-1
+            }
+        }
+        if (start <= end)
+            result.add(start..end)
+        return result
+    }
+
+    private fun List<LongRange>.fillEmptyRangesInDestination(): List<LongRange> {
+        var result = this.toMutableList()
+        var start = 0L
+        this.sortedBy { aRange -> aRange.first }.forEach { currentRange ->
+            if (start < currentRange.first) {
+                result += (start .. (currentRange.first-1))
+            }
+            start = currentRange.last+1
+        }
+        return result
+    }
+
+    private fun Transformer.fillEmptyRangesInDestination(): Transformer {
+        var destinationResult = mutableListOf<LongRange>()
+        var sourceResult = mutableListOf<LongRange>()
+        var start = 0L
+        this.transformNumberList.sortedBy { t -> t.destinationRange.first }.forEach { t ->
+            if (start < t.destinationRange.first) {
+                destinationResult += (start .. (t.destinationRange.first-1))
+                sourceResult += (start .. (t.destinationRange.first-1))
+            } else {
+                destinationResult += t.destinationRange
+                sourceResult += t.sourceRange
+            }
+            start = t.destinationRange.last+1
+        }
+        return Transformer(
+            source=this.source,
+            destination=this.destination,
+            transformNumberList=List(destinationResult.size) { index -> TransformNumber(sourceResult[index], destinationResult[index] ) }
+        )
+    }
+
+    //
+    // brute-force works, surprisingly enough. It took about 9-10 minutes,
+    // I run it, while developing a better alternative, but it found the result (way) before
+    // my alternative solution was developed ;-)
+    //
     private fun partTwoBruteforce(): Any {
         var smallest = Long.MAX_VALUE
 
@@ -73,48 +171,8 @@ class Day05(test: Boolean) : PuzzleSolverAbstract(test) {
         return smallest
     }
 
-
-    private fun seedToLocation(seedNumber: Long): Long {
-        var current = "seed"
-        var currentNumber = seedNumber
-
-        while (transformerMap.contains(current)) {
-//            print("$current $currentNumber")
-            currentNumber = transformerMap[current]!!.transform(currentNumber)
-            current = transformerMap[current]!!.destination
-//            println(" --> $currentNumber")
-        }
-//        println("$current $currentNumber")
-        return currentNumber
-    }
 }
 
-fun Long.inRanges(checkRanges: List<LongRange>) : Boolean {
-    return checkRanges.any{range -> this in range}
-}
-
-fun destinationRangesToSource(inRange: LongRange, compareRanges: List<TransformNumber>): List<LongRange> {
-    val result = mutableListOf<LongRange>()
-    var start = inRange.first
-    var end = inRange.last
-    compareRanges.forEach {compareRange ->
-        if (start <= end && start in compareRange.destinationRange && end in compareRange.destinationRange) {
-            result.add(compareRange.sourceRange.first + (start-compareRange.destinationRange.first)..compareRange.sourceRange.first + (end-compareRange.destinationRange.first))
-            return result
-        }
-        if (start <= end && start in compareRange.destinationRange) {
-            result.add(compareRange.sourceRange.first + (start-compareRange.destinationRange.first)..compareRange.sourceRange.last)
-            start = compareRange.destinationRange.last+1
-        }
-        if (start <= end && end in compareRange.destinationRange) {
-            result.add(compareRange.sourceRange.first ..compareRange.sourceRange.first + (end-compareRange.destinationRange.first))
-            end = compareRange.destinationRange.first-1
-        }
-    }
-    if (start <= end)
-        result.add(start..end)
-    return result
-}
 
 
 data class Transformer(val source: String, val destination: String, val transformNumberList: List<TransformNumber>) {
