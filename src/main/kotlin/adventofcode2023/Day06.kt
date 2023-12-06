28bb2c79148ef008f7307c7a449c9801f0073745
Martin Schijf <81872457+mschijf@users.noreply.github.com>
2023-12-06 06:46:45 +0100

    Day 6 - improved performance by not creating an intermeidate list of results, but count the beating records immediately

--- src/main/kotlin/adventofcode2023/Day06.kt
+++ src/main/kotlin/adventofcode2023/Day06.kt
@@ -11,19 +11,20 @@ class Day06(test: Boolean) : PuzzleSolverAbstract(test, hasInputFile = true) {
         val distanceList = inputLines.last().substringAfter("Distance:").trim().split("\\s+".toRegex()).map{it.toLong()}
 
         return timeList
-            .mapIndexed{ index, time -> play(time).count{it > distanceList[index]} }
+            .mapIndexed{ index, time -> countBeatingRecords(time, distanceList[index]) }
             .reduce { acc, i -> acc*i }
     }
 
-    private fun play(time: Long):List<Long> {
-        return (0L..time).map{t -> t*(time-t)}
+    private fun countBeatingRecords(time: Long, record: Long): Int {
+        return (0L..time).count { t -> t*(time-t) > record }
     }
 
+    //        227850
+    //result: 42948149
     override fun resultPartTwo(): Any {
         val time = inputLines.first().substringAfter("Time:").trim().replace(" ", "").toLong()
         val distance = inputLines.last().substringAfter("Distance:").trim().replace(" ", "").toLong()
-
-        return play(time).count{t -> t*(time-t) > distance}
+        return countBeatingRecords(time, distance)
     }
 }
 
