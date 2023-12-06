0360adf1e807853df062dfd16c7f4bfbe16b80a8
Martin Schijf <81872457+mschijf@users.noreply.github.com>
2023-12-06 06:54:21 +0100

    Day 6 - improved performance by only looping over half of the times

--- src/main/kotlin/adventofcode2023/Day06.kt
+++ src/main/kotlin/adventofcode2023/Day06.kt
@@ -6,6 +6,7 @@ fun main() {
 
 class Day06(test: Boolean) : PuzzleSolverAbstract(test, hasInputFile = true) {
 
+    //result: 227850
     override fun resultPartOne(): Any {
         val timeList = inputLines.first().substringAfter("Time:").trim().split("\\s+".toRegex()).map{it.toLong()}
         val distanceList = inputLines.last().substringAfter("Distance:").trim().split("\\s+".toRegex()).map{it.toLong()}
@@ -15,11 +16,17 @@ class Day06(test: Boolean) : PuzzleSolverAbstract(test, hasInputFile = true) {
             .reduce { acc, i -> acc*i }
     }
 
+    // you can reduce time, by only looping over half of the times,
+    // since 3*(10-3) = 7*(10-7), you only have to look for 0,1,2,3,4,5
+
     private fun countBeatingRecords(time: Long, record: Long): Int {
-        return (0L..time).count { t -> t*(time-t) > record }
+        return if (time % 2 == 0L)
+            (0L..time/2 - 1).count { t -> t*(time-t) > record } * 2 + (if ((time-time/2)*(time/2) > record) 1 else 0)
+        else
+            (0L..time/2).count { t -> t*(time-t) > record } * 2
+        //return (0L..time).count { t -> t*(time-t) > record } * 2
     }
 
-    //        227850
     //result: 42948149
     override fun resultPartTwo(): Any {
         val time = inputLines.first().substringAfter("Time:").trim().replace(" ", "").toLong()
