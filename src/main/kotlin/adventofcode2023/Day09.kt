9ddbfbf43369ded25e3b087a5b93372b2fc6dd20
Martin Schijf <81872457+mschijf@users.noreply.github.com>
2023-12-09 07:34:55 +0100

    Day 9 -> refactored - making it a bit more readable (and a bit less performing)

--- src/main/kotlin/adventofcode2023/Day09.kt
+++ src/main/kotlin/adventofcode2023/Day09.kt
@@ -9,47 +9,40 @@ class Day09(test: Boolean) : PuzzleSolverAbstract(test, hasInputFile = true) {
     private val historyLines = inputLines.map{line -> line.split("\\s+".toRegex()).map{ word -> word.toInt() }}
 
     override fun resultPartOne(): Any {
-        return historyLines.sumOf{ it.predictNextValue() }
+        return historyLines.sumOf{ line -> line.predictNextValue() }
     }
 
     override fun resultPartTwo(): Any {
-        return historyLines.sumOf{ it.predictFirstValue() }
+        return historyLines.sumOf{ line -> line.predictFirstValue() }
     }
 
-    private fun List<Int>.differenceSequence() : List<List<Int>> {
-        val differenceSequence = mutableListOf<List<Int>>()
+    //
+    // creates a list of lists.
+    // Each list has the differences of two adjacent fields of the previous list
+    //
+
+    private fun List<Int>.sequenceOfdifferences() : List<List<Int>> {
+        val sequenceOfDifferences = mutableListOf<List<Int>>()
         var next = this
         while (next.any{it != 0}) {
-            differenceSequence.add(next)
+            sequenceOfDifferences.add(next)
             next = next.zipWithNext { a, b -> b-a }
         }
-        return differenceSequence
+        return sequenceOfDifferences
     }
 
     private fun List<Int>.predictNextValue(): Int {
         return this
-            .differenceSequence()
-            .foldRight(0){intList, acc -> acc + intList.last()}
-//
-//        val extrapolateList = this.differenceSequence()
-//        var newValue = 0
-//        extrapolateList.reversed().forEach {
-//            newValue = it.last() + newValue
-//        }
-//        return newValue
+            .sequenceOfdifferences()
+            .map {it.last()}
+            .foldRight(0){lastValue, acc -> acc + lastValue}
     }
 
     private fun List<Int>.predictFirstValue(): Int {
         return this
-            .differenceSequence()
-            .foldRight(0){intList, acc -> intList.first() - acc}
-//
-//        val extrapolateList = this.differenceSequence()
-//        var newValue = 0
-//        extrapolateList.reversed().forEach {
-//            newValue = it.first() - newValue
-//        }
-//        return newValue
+            .sequenceOfdifferences()
+            .map {it.first()}
+            .foldRight(0){firstValue, acc -> firstValue - acc}
     }
 
 }
