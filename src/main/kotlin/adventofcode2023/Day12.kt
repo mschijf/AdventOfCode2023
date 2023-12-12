31179666eeb095df93ebffb1d7b5dae8efdb6b70
Martin Schijf <81872457+mschijf@users.noreply.github.com>
2023-12-12 21:27:29 +0100

    Day 12: meer gerommel, maar betere method

--- src/main/kotlin/adventofcode2023/Day12.kt
+++ src/main/kotlin/adventofcode2023/Day12.kt
@@ -1,7 +1,7 @@
 package adventofcode2023
 
 fun main() {
-    Day12(test=true).showResult()
+    Day12(test=false).showResult()
 }
 
 class Day12(test: Boolean) : PuzzleSolverAbstract(test, hasInputFile = true) {
@@ -11,16 +11,13 @@ class Day12(test: Boolean) : PuzzleSolverAbstract(test, hasInputFile = true) {
         val conditionRecordLines = inputLines.map { it.split("\\s+".toRegex()).first()}
         val conditionRecordGroups = inputLines.map { it.split("\\s+".toRegex()).last().split(",").map{it.toInt()}}
         return execute(conditionRecordLines, conditionRecordGroups)
-//        return conditionRecordLines.mapIndexed { index, s -> s.permutationCount(conditionRecordGroups[index]) }.sum()
     }
 
-//    override fun resultPartTwo(): Any {
-//        val conditionRecordLines = inputLines.map { it.split("\\s+".toRegex()).first().copyFive("?")}
-//        val conditionRecordGroups = inputLines.map { it.split("\\s+".toRegex()).last().copyFive(",").split(",").map{it.toInt()}}
-//        return conditionRecordLines.filter{it.startsWith("?") && it.endsWith("?")}.count()
-//        return execute(conditionRecordLines, conditionRecordGroups)
-//        //return conditionRecordLines.mapIndexed { index, s -> s.permutationCount(conditionRecordGroups[index]) }.sum()
-//    }
+    override fun resultPartTwo(): Any {
+        val conditionRecordLines = inputLines.map { it.split("\\s+".toRegex()).first().copyFive("?")}
+        val conditionRecordGroups = inputLines.map { it.split("\\s+".toRegex()).last().copyFive(",").split(",").map{it.toInt()}}
+        return execute(conditionRecordLines, conditionRecordGroups)
+    }
 
 
     private fun String.copyFive(delimiter: String): String {
@@ -30,8 +27,7 @@ class Day12(test: Boolean) : PuzzleSolverAbstract(test, hasInputFile = true) {
     private fun execute(conditionRecordLines: List<String>, conditionRecordGroups: List<List<Int>>): Long {
         var sum = 0L
         conditionRecordLines.forEachIndexed { index, s ->
-//            val count = s.permutationCount( conditionRecordGroups[index], "", 0)
-            val count = s.permutationCountV2( conditionRecordGroups[index], "", 0)
+            val count = calculate(s, conditionRecordGroups[index])
             println("$index -> $count")
             sum += count
         }
@@ -39,98 +35,153 @@ class Day12(test: Boolean) : PuzzleSolverAbstract(test, hasInputFile = true) {
     }
 
 
-//    private fun String.permutationCount(conditionRecordGroup:List<Int>, s: String="", index: Int = 0): Int {
-//        val tmpConditionGroup = s.makeGroups()
-//        if (s.length == this.length) {
-//            return if (conditionRecordGroup == tmpConditionGroup) 1 else 0
-//        }
-//        if (!conditionRecordGroup.startsWith(tmpConditionGroup)) {
-//            return 0
-//        }
-//
-//        if (this[index] == '?') {
-//            return this.permutationCount(conditionRecordGroup, s+"#", index+1) + this.permutationCount(conditionRecordGroup, s+".", index+1)
-//        } else {
-//            return this.permutationCount(conditionRecordGroup, s+this[index], index+1)
-//        }
-//    }
+    private fun calculate(inputString: String, conditionRecordGroup: List<Int>, lastChar: Char=' ', hekjesGehad: Int = 0,
+                          cache:MutableMap<Triple<String, Int, List<Int>>, Long> = mutableMapOf() ): Long {
 
-    private fun String.permutationCount(conditionRecordGroup:List<Int>, s: String, stringIndex: Int): Long {
-        if (s.length == this.length) {
-            val tmpConditionGroup = s.makeGroups()
-            return if (conditionRecordGroup == tmpConditionGroup) 1 else 0
+        if (conditionRecordGroup.isEmpty()) {
+            return if (inputString.contains("#")) 0 else 1L
         }
-        val tmpConditionGroup = s.makeGroups()
-        if (!conditionRecordGroup.startsWith(tmpConditionGroup)) {
+
+        if (inputString.isEmpty()) {
+            return if (conditionRecordGroup.size == 1 && conditionRecordGroup.first() == hekjesGehad) 1 else 0
+        }
+
+        if (hekjesGehad > conditionRecordGroup.first()) {
             return 0
         }
 
-        val tmp = if (this[stringIndex] == '?') {
-             this.permutationCount(conditionRecordGroup, s+"#", stringIndex+1) +
-                     this.permutationCount(conditionRecordGroup, s+".", stringIndex+1)
+        val cacheKey = Triple(inputString, hekjesGehad, conditionRecordGroup)
+        if (cache.contains(cacheKey)) {
+            return cache[cacheKey]!!
+        }
+
+        val currentChar = inputString.first()
+        val remainder = inputString.drop(1)
+        val tmp = if (currentChar == '?') {
+            val metHekje = calculate(remainder, conditionRecordGroup, '#', hekjesGehad+1)
+
+            val metPuntje = if (lastChar == '#') {
+                if (hekjesGehad != conditionRecordGroup.first()) {
+                    0
+                } else {
+                    calculate(remainder, conditionRecordGroup.drop(1), '.', 0)
+                }
+            } else {
+                calculate(remainder, conditionRecordGroup, '.', 0)
+            }
+
+            metHekje + metPuntje
         } else {
-            this.permutationCount(conditionRecordGroup, s+this[stringIndex], stringIndex+1)
-        }
-        return tmp
-    }
-
-
-    private fun String.permutationCountV2(conditionRecordGroup:List<Int>, s: String, stringIndex: Int, cache: MutableMap<Pair<Int, Int>, Long> = mutableMapOf<Pair<Int, Int>, Long>()): Long {
-        val tmpConditionGroup = s.makeGroups()
-        if (s.length == this.length) {
-            return if (conditionRecordGroup == tmpConditionGroup) 1 else 0
-        }
-        if (!conditionRecordGroup.startsWith(tmpConditionGroup)) {
-            return 0
-        }
-
-        if (s.endsWith(".")) {
-            val cacheKey = Pair(stringIndex, conditionRecordGroup.size - tmpConditionGroup.size)
-            if (cache.contains(cacheKey)) {
-                return cache[cacheKey]!!
+            if (lastChar == '#' && currentChar == '.') {
+                if (hekjesGehad != conditionRecordGroup.first()) {
+                    0
+                } else {
+                    calculate(remainder, conditionRecordGroup.drop(1), currentChar, 0)
+                }
+            } else {
+                calculate(remainder, conditionRecordGroup, currentChar, if (currentChar == '#') hekjesGehad + 1 else 0)
             }
         }
-
-        val tmp = if (this[stringIndex] == '?') {
-            this.permutationCountV2(conditionRecordGroup, s+"#", stringIndex+1, cache) +
-                    this.permutationCountV2(conditionRecordGroup, s+".", stringIndex+1, cache)
-        } else {
-            this.permutationCountV2(conditionRecordGroup, s+this[stringIndex], stringIndex+1, cache)
-        }
-
-        if (s.endsWith(".")) {
-            val cacheKey = Pair(stringIndex, conditionRecordGroup.size - tmpConditionGroup.size)
-            cache[cacheKey] = tmp
-        }
+        cache[cacheKey] = tmp
         return tmp
-    }
-
-
-
-
-
-    private fun List<Int>.startsWith(other: List<Int>): Boolean {
-        if (other.size == 0) {
-            return true
-        }
-
-        if (other.size == 1) {
-            return other[0] <= this[0]
-        }
-
-        if (other.size > this.size)
-            return false
-
-        for (index in 0 .. other.size-2) {
-            if (this[index] != other[index])
-                return false
-        }
-        return true
-    }
-
-    private fun String.makeGroups(): List<Int> {
-        return this.split(".").map{it.length}.filterNot { it ==  0 }
     }
 }
 
 
+
+
+//    /** The condition record of a spring. If unknown, can be assumed to be either of the other two. */
+//    private enum class Condition { Operational, Damaged, Unknown }
+//
+//    /**
+//     * An entry in the spring ledger.
+//     * @property springs  The condition of the springs in this entry.
+//     * @property checksum The number and size of the runs of damaged springs.
+//     */
+//    private data class ConditionRecord(val springs: List<Condition>, val checksum: List<Int>) {
+//
+//        /**
+//         * The number of valid solutions tanking into account [Unknown] spring conditions, if any.
+//         * If there are no unknown [springs], this value will be 1 if the [checksum] is valid, or 0 otherwise.
+//         */
+//        fun validSolutions(cache: MutableMap<ConditionRecord, Long>): Long {
+//            // If the valid solution has already been computed, reuse it.
+//            cache[this]?.let { return it }
+//
+//            // If no springs left, either no checksums left to satisfy, or the record is invalid.
+//            if (springs.isEmpty()) return if (checksum.isEmpty()) 1 else 0
+//
+//            // If some springs left, but no checksums, then all remaining springs must be operational.
+//            // Unknowns can be assumed as operational, but if any known damaged left, the record is invalid.
+//            if (checksum.isEmpty()) return if (springs.contains(Condition.Damaged)) 0 else 1
+//
+//            val spring = springs.first()
+//            val expectedDamaged = checksum.first()
+//
+//            // If the next spring could be operational, then nothing to check, just ignore it.
+//            val solutionsIfOperational =
+//                if (spring != Condition.Damaged) ConditionRecord(springs.drop(1), checksum).validSolutions(cache)
+//                else 0
+//
+//            // If the next spring could be damaged, check that all conditions for a valid damaged streak are met:
+//            // - There must be at least as many springs left as the next expected checksum value.
+//            // - The next checksum value's worth of springs must all be (or can be disambiguated into being) damaged.
+//            // - After the streak, either there are no springs left to check, or the streak is properly ended by an
+//            //   operational spring (or one that can be disambiguated as such).
+//            // If these conditions are met, then we should skip the current damaged streak (and maybe its terminator)
+//            // entirely, satisfy the checksum, and process the remaining springs.
+//            val solutionsIfSpringDamaged = when {
+//                spring == Condition.Operational -> 0
+//                expectedDamaged > springs.size -> 0
+//                springs.take(expectedDamaged).any { it == Condition.Operational } -> 0
+//                springs.size != expectedDamaged && springs[expectedDamaged] == Condition.Damaged -> 0
+//                else -> ConditionRecord(springs.drop(expectedDamaged + 1), checksum.drop(1)).validSolutions(cache)
+//            }
+//
+//            return solutionsIfOperational
+//                .plus(solutionsIfSpringDamaged)
+//                .also { result -> cache[this] = result }
+//        }
+//    }
+//
+//    /**
+//     * Parse the [input] and return the list of condition records.
+//     * @param input  The puzzle input.
+//     * @param unfold Whether to unfold the records before reading them.
+//     */
+//    private fun parseInput(input: String, unfold: Boolean): List<ConditionRecord> = parse {
+//        val lineRegex = Regex("""^([#.?]+) (\d[\d,]*\d)$""")
+//
+//        fun String.unfold(separator: Char): String = buildString(length.inc() + 5) {
+//            val original = this@unfold
+//            append(original)
+//            repeat(4) { append(separator, original) }
+//        }
+//
+//        input
+//            .lineSequence()
+//            .map { lineRegex.matchEntire(it)!!.destructured }
+//            .map { (record, checksum) ->
+//                if(unfold) record.unfold('?') to checksum.unfold(',')
+//                else record to checksum
+//            }
+//            .map { (record, checksum) ->
+//                ConditionRecord(
+//                    springs = record.map {
+//                        when(it) {
+//                            '.' -> Condition.Operational
+//                            '#' -> Condition.Damaged
+//                            '?' -> Condition.Unknown
+//                            else -> error("Impossible state")
+//                        }
+//                    },
+//                    checksum = checksum.split(',').map(String::toInt),
+//                )
+//            }
+//            .toList()
+//    }
+//
+//    private fun List<ConditionRecord>.solve(): Long {
+//        val cache: MutableMap<ConditionRecord, Long> = mutableMapOf()
+//        return sumOf { it.validSolutions(cache) }
+//    }
