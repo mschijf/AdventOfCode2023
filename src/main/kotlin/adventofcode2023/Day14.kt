fce8fcf657a3b76e192703efe4e2b3cc5b49c18c
Martin Schijf <81872457+mschijf@users.noreply.github.com>
2023-12-16 07:55:03 +0100

    Day 14: commented out the nice coding (but slow) solution

--- src/main/kotlin/adventofcode2023/Day14.kt
+++ src/main/kotlin/adventofcode2023/Day14.kt
@@ -16,100 +16,100 @@ fun main() {
 class Day14(test: Boolean) : PuzzleSolverAbstract(test, hasInputFile = true) {
 
 
-    private val grid = inputAsGrid()
-    private val cubeRocks = grid.filterValues{it == '#'}.keys
-    private val roundedRocks = grid.filterValues{it == 'O'}.keys
-    private val rowCount = inputLines.size
-    private val colCount = inputLines.firstOrNull()?.length ?:0
-
-    override fun resultPartOne(): Any {
-        return roundedRocks.rollNorth().platformLoad()
-    }
-
-    private fun absoluteTop(from: Point): Int {
-        return cubeRocks.filter{it.x == from.x && it.y < from.y}.maxOfOrNull { it.y }?:-1
-    }
-    private fun absoluteBottom(from: Point): Int {
-        return cubeRocks.filter{it.x == from.x && it.y > from.y}.minOfOrNull { it.y }?:rowCount
-    }
-    private fun absoluteLeft(from: Point): Int {
-        return cubeRocks.filter{it.y == from.y && it.x < from.x}.maxOfOrNull { it.x }?:-1
-    }
-    private fun absoluteRight(from: Point): Int {
-        return cubeRocks.filter{it.y == from.y && it.x > from.x}.minOfOrNull { it.x }?:colCount
-    }
-
-    private fun Set<Point>.rollNorth(): Set<Point> {
-        return this.map { from ->
-            val absTop = absoluteTop(from)
-            val countAbove = this.count{it.x == from.x && it.y < from.y && absTop < it.y }
-            val freeSpace = (from.y-absTop-1)-countAbove
-            pos(from.x, from.y - freeSpace)
-        }.toSet()
-    }
-
-
-    private fun Set<Point>.rollSouth(): Set<Point> {
-        return this.map { from ->
-            val absBottom = absoluteBottom(from)
-            val countBelow = this.count { it.x == from.x && it.y > from.y && absBottom > it.y }
-            val freeSpace = (absBottom - from.y - 1) - countBelow
-            pos(from.x, from.y + freeSpace)
-        }.toSet()
-    }
-
-
-    private fun Set<Point>.rollWest(): Set<Point> {
-        return this.map { from ->
-            val absLeft = absoluteLeft(from)
-            val countLeft = this.count { it.y == from.y && it.x < from.x && absLeft < it.x }
-            val freeSpace = (from.x - absLeft - 1) - countLeft
-            pos(from.x - freeSpace, from.y)
-        }.toSet()
-    }
-
-    private fun Set<Point>.rollEast(): Set<Point> {
-        return this.map { from ->
-            val absRight = absoluteRight(from)
-            val countRight = this.count { it.y == from.y && it.x > from.x && absRight > it.x }
-            val freeSpace = (absRight - from.x - 1) - countRight
-            pos(from.x + freeSpace, from.y)
-        }.toSet()
-    }
-
-    private fun Set<Point>.cycle(): Set<Point> {
-        return this.rollNorth().rollWest().rollSouth().rollEast()
-    }
-
-    private fun Set<Point>.platformLoad(): Int {
-        return this.sumOf { rowCount - it.y }
-    }
-
-    /**
-     * Mooier leesbaar, maar wel een stuk trager
-     */
-    private val history : MutableList<Set<Point>> = mutableListOf()
-    private val loadHistory : MutableList<Int> = mutableListOf()
-    override fun resultPartTwo(): Any {
-        var platform = roundedRocks
-        history.add(platform)
-        loadHistory.add(platform.platformLoad())
-        repeat(1_000_000_000) {
-            platform = platform.cycle()
-            if (platform in history) {
-                val index = history.indexOf(platform)
-                val cycleTime = (it+1-index)
-                val remainder = (1_000_000_000 - (index)) % cycleTime
-                val totalRuns = index + remainder
-                println("REPEATER after $it at $index, cycletime: $cycleTime, totalruns = $totalRuns")
-                return (loadHistory[index+remainder])
-            }
-            println(it)
-            history.add(platform)
-            loadHistory.add(platform.platformLoad())
-        }
-        return -1
-    }
+//    private val grid = inputAsGrid()
+//    private val cubeRocks = grid.filterValues{it == '#'}.keys
+//    private val roundedRocks = grid.filterValues{it == 'O'}.keys
+//    private val rowCount = inputLines.size
+//    private val colCount = inputLines.firstOrNull()?.length ?:0
+//
+//    override fun resultPartOne(): Any {
+//        return roundedRocks.rollNorth().platformLoad()
+//    }
+//
+//    private fun absoluteTop(from: Point): Int {
+//        return cubeRocks.filter{it.x == from.x && it.y < from.y}.maxOfOrNull { it.y }?:-1
+//    }
+//    private fun absoluteBottom(from: Point): Int {
+//        return cubeRocks.filter{it.x == from.x && it.y > from.y}.minOfOrNull { it.y }?:rowCount
+//    }
+//    private fun absoluteLeft(from: Point): Int {
+//        return cubeRocks.filter{it.y == from.y && it.x < from.x}.maxOfOrNull { it.x }?:-1
+//    }
+//    private fun absoluteRight(from: Point): Int {
+//        return cubeRocks.filter{it.y == from.y && it.x > from.x}.minOfOrNull { it.x }?:colCount
+//    }
+//
+//    private fun Set<Point>.rollNorth(): Set<Point> {
+//        return this.map { from ->
+//            val absTop = absoluteTop(from)
+//            val countAbove = this.count{it.x == from.x && it.y < from.y && absTop < it.y }
+//            val freeSpace = (from.y-absTop-1)-countAbove
+//            pos(from.x, from.y - freeSpace)
+//        }.toSet()
+//    }
+//
+//
+//    private fun Set<Point>.rollSouth(): Set<Point> {
+//        return this.map { from ->
+//            val absBottom = absoluteBottom(from)
+//            val countBelow = this.count { it.x == from.x && it.y > from.y && absBottom > it.y }
+//            val freeSpace = (absBottom - from.y - 1) - countBelow
+//            pos(from.x, from.y + freeSpace)
+//        }.toSet()
+//    }
+//
+//
+//    private fun Set<Point>.rollWest(): Set<Point> {
+//        return this.map { from ->
+//            val absLeft = absoluteLeft(from)
+//            val countLeft = this.count { it.y == from.y && it.x < from.x && absLeft < it.x }
+//            val freeSpace = (from.x - absLeft - 1) - countLeft
+//            pos(from.x - freeSpace, from.y)
+//        }.toSet()
+//    }
+//
+//    private fun Set<Point>.rollEast(): Set<Point> {
+//        return this.map { from ->
+//            val absRight = absoluteRight(from)
+//            val countRight = this.count { it.y == from.y && it.x > from.x && absRight > it.x }
+//            val freeSpace = (absRight - from.x - 1) - countRight
+//            pos(from.x + freeSpace, from.y)
+//        }.toSet()
+//    }
+//
+//    private fun Set<Point>.cycle(): Set<Point> {
+//        return this.rollNorth().rollWest().rollSouth().rollEast()
+//    }
+//
+//    private fun Set<Point>.platformLoad(): Int {
+//        return this.sumOf { rowCount - it.y }
+//    }
+//
+//    /**
+//     * Mooier leesbaar, maar wel een stuk trager
+//     */
+//    private val history : MutableList<Set<Point>> = mutableListOf()
+//    private val loadHistory : MutableList<Int> = mutableListOf()
+//    override fun resultPartTwo(): Any {
+//        var platform = roundedRocks
+//        history.add(platform)
+//        loadHistory.add(platform.platformLoad())
+//        repeat(1_000_000_000) {
+//            platform = platform.cycle()
+//            if (platform in history) {
+//                val index = history.indexOf(platform)
+//                val cycleTime = (it+1-index)
+//                val remainder = (1_000_000_000 - (index)) % cycleTime
+//                val totalRuns = index + remainder
+//                println("REPEATER after $it at $index, cycletime: $cycleTime, totalruns = $totalRuns")
+//                return (loadHistory[index+remainder])
+//            }
+//            println(it)
+//            history.add(platform)
+//            loadHistory.add(platform.platformLoad())
+//        }
+//        return -1
+//    }
 
     //-----------------------------------------------------------------------------------------------------------------
 
@@ -121,34 +121,38 @@ class Day14(test: Boolean) : PuzzleSolverAbstract(test, hasInputFile = true) {
 
     private val platform = inputLines.map { line -> line.map { ch ->  ch}.toMutableList() }
 
-//    override fun resultPartOne(): Any {
-//        rollNorth()
-//        return platformTotalLoad()
-//    }
+    override fun resultPartOne(): Any {
+        rollNorth()
+        return platformTotalLoad()
+    }
 
-//    val history : MutableList<Int> = mutableListOf()
-//    val loadHistory : MutableList<Int> = mutableListOf()
-//    override fun resultPartTwo(): Any {
-//        history.add(platformValue())
-//        loadHistory.add(platformTotalLoad())
-//        repeat(1_000_000_000) {
-//            cycle()
-//            if (platformValue() in history) {
-//                val index = history.indexOf(platformValue())
-//                val cycleTime = (it+1-index)
-//                val restje = (1_000_000_000 - (index)) % cycleTime
-//                val totalRuns = index + restje
+    override fun resultPartTwo(): Any {
+        return runTillRepetition()
+    }
+
+    private fun runTillRepetition(): Int {
+        val history : MutableList<Int> = mutableListOf()
+        val loadHistory : MutableList<Int> = mutableListOf()
+        history.add(platformValue())
+        loadHistory.add(platformTotalLoad())
+        repeat(1_000_000_000) {
+            cycle()
+            if (platformValue() in history) {
+                val index = history.indexOf(platformValue())
+                val cycleTime = (it+1-index)
+                val restje = (1_000_000_000 - (index)) % cycleTime
+                val totalRuns = index + restje
 //                println("REPEATER after $it at $index, cycletime: $cycleTime, totalruns = $totalRuns")
 //                println(loadHistory[index+restje])
-//                return "KLAAR"
-//            }
-//            history.add(platformValue())
-//            loadHistory.add(platformTotalLoad())
+                return platformTotalLoad()
+            }
+            history.add(platformValue())
+            loadHistory.add(platformTotalLoad())
 //            if (it % 1_000_000 == 0)
 //                println("done $it cycles" )
-//        }
-//        return platformTotalLoad()
-//    }
+        }
+        return -1
+    }
 
     private fun platformValue() : Int {
         var value = 0
