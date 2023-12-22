060b8811eb75a86ea80eb5b07b754914179e235b
Martin Schijf <81872457+mschijf@users.noreply.github.com>
2023-12-22 21:44:11 +0100

    General classes Rectangle, Line and INtRange

---
+++ src/main/kotlin/tool/coordinate/twodimensional/IntRangeFunctions.kt
@@ -0,0 +1,5 @@
+package tool.coordinate.twodimensional
+
+fun IntRange.overlaps(other:IntRange): Boolean {
+    return (this.first in other || this.last in other || other.first in this || other.last in this)
+}
